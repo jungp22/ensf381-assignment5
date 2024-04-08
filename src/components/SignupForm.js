31f6976a6e0d7515102c7928232c71e085248fc9
Jung-Hwan Park <jungpark.2002@gmail.com>
2024-04-08 07:07:03 -0600

    implemented signup to frontend

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,51 +1,62 @@
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 
 const SignupForm = ({ toggleForm }) => {
+  
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [signedUp, setSignedUp] = useState(false);
+  const navigate = useNavigate();
+
+  function handleSignUp() {
+    fetch("http://127.0.0.1:5000/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username: username, password: password }),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        if (response.authenticated) {
+          setSignedUp(true);
+          setMessage("Authentication successful");
+        } else {
+          setSignedUp(false);
+          setMessage("Authentication failed. Incorrect username or password.");
+        }
+      })
+      .catch((error) =>
+        setMessage("Authentication failed. Incorrect username or password.")
+      );
+  }
+  if (signedUp) {
+    // Redirect to another page after successful authentication
+    navigate("/");
+  }
+
+
   return (
     <div>
-        <h1>Signup</h1>
-        <form name="signup" onsubmit="validateForm(event)">
-          <label for="username">Username:</label>
-          
-          <input
-            type="text"
-            id="username"
-            name="username"
-            required
-            placeholder="Enter your username"
-          />
-          <br/>
-          <label for="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            required
-            placeholder="Enter your password"
-          />
-          <br/>
-          <label for="password-confirm">Confirm Password:</label>
-          <input
-            type="password"
-            id="confirm-password"
-            name="confirm-password"
-            required
-            placeholder="Confirm your password"
-          />
-          <br/>
-          <label for="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            required
-            placeholder="Enter your email"
-          />
-          <br/>
+      <p>{message}</p>
+      <label>
+        Username:
+        <input type="text" onChange={(e) => setUsername(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <br />
+      <button type="submit" class="login-btn" onClick={handleSignUp}>Login</button>
+      <br/>
+      <button type="button" onClick={toggleForm}>
+        Switch to Login
+      </button>
 
-          <button type="submit" class="signup-btn">Login</button><br/>
-          <button type="button" onClick={toggleForm}>Switch to Login</button>
-
-        </form>
     </div>
   );
 };
