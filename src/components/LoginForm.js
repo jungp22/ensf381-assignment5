5ea66c63fb2acddb945c185168d7c91b86429535
Jung-Hwan Park <jungpark.2002@gmail.com>
2024-04-08 06:34:16 -0600

    integrated login function

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,32 +1,58 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const LoginForm = ({ toggleForm }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [authenticated, setAuthenticated] = useState(false);
+  const navigate = useNavigate();
+  function handleAuthentication() {
+    fetch("http://127.0.0.1:5000/authenticate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username: username, password: password }),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        if (response.authenticated) {
+          setAuthenticated(true);
+          setMessage("Authentication successful");
+        } else {
+          setAuthenticated(false);
+          setMessage("Authentication failed. Incorrect username or password.");
+        }
+      })
+      .catch((error) =>
+        setMessage("Authentication failed. Incorrect username or password.")
+      );
+  }
+  if (authenticated) {
+    // Redirect to another page after successful authentication
+    navigate("/dashboard");
+  }
+
   return (
     <div>
-        <h1>Login</h1>
-        <form name="login" onsubmit="validateForm(event)">
-          <label for="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            required
-            placeholder="Enter your username"
-          />
-          <br/>
-          <label for="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            required
-            placeholder="Enter your password"
-          />
-          <br/>
+      <h1>Login</h1>
+      <label>
+        Username:
+        <input type="text" onChange={(e) => setUsername(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <br />
+      <button type="submit" class="login-btn" onClick={handleAuthentication}>Login</button>
+      <p>{message}</p>
 
-          <button type="submit" class="login-btn">Login</button>
-          <br/>
-          <button type="button" onClick={toggleForm}>Switch to Signup</button>
-        </form>
+      <button type="button" onClick={toggleForm}>
+        Switch to Signup
+      </button>
     </div>
   );
 };
